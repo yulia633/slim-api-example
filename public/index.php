2815bcf8a8284fdfd81d79bf10db747de6d30ff6
yulia633 <ignat633yulia@yandex.ru>
2021-05-20 01:20:16 +0300

    response json

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use App\Model\User;
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
@@ -16,8 +17,19 @@ $app->get('/', function (Request $request, Response $response) {
     return $response;
 });
 
-/* to do*/
 // Показать все записи из БД
+$app->get('/users', function (Request $request, Response $response) {
+    // $users = new User();
+    $users = [
+        ['name' => 'Julia', 'email' => '123@goog'],
+        ['name' => 'Tina', 'email' => '567@goog'],
+    ];
+    $response->getBody()->write(json_encode($users));
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+});
+
+/* to do*/
 // Показать запись по ID
 // Добавить запись в БД
 // Редактировать запись в БД по ID
