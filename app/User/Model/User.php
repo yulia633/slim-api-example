d6cb9363823ecd82eac6b7e4586d52c1dbcfaf9d
feycot <feycot@gmail.com>
2021-05-20 20:56:44 +0500

    pair coding

---
+++ app/User/Model/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\User\Model;
+
+class User
+{
+    public string $username;
+    public ?string $id = null;
+    public string $email;
+
+    public function __construct(string $username, string $email)
+    {
+        $this->username = $username;
+        $this->email = $email;
+    }
+
+    public static function fromArray(array $data): static
+    {
+        $user = new self($data['username'], $data['email']);
+        $user->id = $data['id'];
+
+        return $user;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+        ];
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,8 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use App\Model\User;
-use App\Db;
+use App\User\Storage\UserJsonStorage;
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
@@ -20,12 +19,53 @@ $app->get('/', function (Request $request, Response $response) {
 
 // Показать все записи из БД
 $app->get('/users', function (Request $request, Response $response) {
-    // $users = new User();
-    $users = new Db('users');
+    $storage = new UserJsonStorage();
+    $users = $storage->all();
+
     $response->getBody()->write(json_encode($users));
-    return $response
-        ->withHeader('Content-Type', 'application/json');
-});
+
+    return $response->withHeader('Content-Type', 'application/json');
+})->setName('users.index');
+
+$app->get('/users/{id}', function (Request $request, Response $response, $args) {
+    $storage = new UserJsonStorage();
+    $id = $args['id'];
+
+    $user = $storage->getById($id);
+
+    $responseBody = [
+        'item' => $user->toArray(),
+        'actions' => [
+            'index' => "http://localhost:8080/users",
+            'show' => "http://localhost:8080/users/{$id}",
+            'delete' => "http://localhost:8080/users/{$id}",
+        ],
+    ];
+
+    $response->getBody()->write(json_encode($responseBody));
+
+    return $response->withHeader('Content-Type', 'application/json');
+})->setName('users.show');
+
+$app->put('/users/{id}', function (Request $request, Response $response, $args) {
+})->setName('users.update');
+
+$app->delete('/users/{id}', function (Request $request, Response $response, $args) {
+})->setName('users.destroy');
+
+// POST http::localhost:8080/users
+$app->post('/users', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+
+    $storage = new UserJsonStorage();
+    $user = $storage->create($data);
+
+    $encodedUser = json_encode($user->toArray());
+    $response->getBody()->write($encodedUser);
+
+    return $response->withHeader('Content-Type', 'application/json');
+})->setName('users.store');
+
 
 /* to do*/
 // Показать запись по ID
