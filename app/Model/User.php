c44c924be6d84dce59302182795d227d0bdb2018
yulia633 <ignat633yulia@yandex.ru>
2021-05-20 13:53:40 +0300

    update user model

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,8 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Model;
+
+use App\Db;
 
 class User
 {
-    //
+    protected $db;
+    
+    public $users;
+
+    public function __construct()
+    {
+        //Db connection
+    }
+
+    public function finfById(string $id)
+    {
+        //
+    }
 }
